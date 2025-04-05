7e8d668a8e9a960a83c6c70e1ae2045e22ad5632
At1z <Adrianlistkiewiczv2@gmail.com>
2025-04-05 11:36:02 +0200

    to do po silowni embedding -> indexing -> searching

---
+++ launching_data_from_psql_example.py
@@ -0,0 +1,23 @@
+import psycopg2
+import os
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import PGVector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+POSTGRES_CONNECTION = os.getenv("POSTGRES_CONNECTION")
+
+def get_data_from_psql():
+    conn = psycopg2.connect(POSTGRES_CONNECTION)
+    cursor = conn.cursor()
+    cursor.execute("SELECT question, answer FROM faq")
+
+    rows = cursor.fetchall()
+    data = [{"question": row[0], "answer": row[1]} for row in rows]
+
+    cursor.close()
+    conn.close()
+    return data

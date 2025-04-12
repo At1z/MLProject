ad4f345dbf5117251a212cc14b8ec7e1d14ea969
At1z <Adrianlistkiewiczv2@gmail.com>
2025-04-12 17:20:50 +0200

    fix prompt

--- ollama.py
+++ ollama.py
@@ -1,15 +1,15 @@
 import requests
 from searching_example import searching
 
-# Modele do testu DeepSeek-R1 (W PIZDE DŁUGO ODPOWIADA),
-def ask_ollama(prompt, model="deepseek-r1:70b"):
+# Modele do testu DeepSeek-R1 ( W PIZDE DŁUGO ODPOWIADA) , TESTUJE BOSSA deepseek-r1:70b (40 GB XD)
+def ask_ollama(prompt, model="DeepSeek-R1"):
 
     docs_scores = searching(prompt)
 
     context = "\n\n".join([doc.page_content for doc, _ in docs_scores])
 
     chat_history = [
-        {"role": "system", "content": "You are a helpful chatbot. Try to amswer in few sentences. Use the following information to answer the user's questions. These documents contain the knowledge you need to assist the user:"},
+        {"role": "system", "content": "You are a helpful chatbot. Try to answer in few sentences. Use the following information to answer the user's questions. These documents contain the knowledge you need to assist the user:"},
         {"role": "system", "content": f"Contextual documents:\n{context}"},
         {"role": "user", "content": prompt}
     ]
